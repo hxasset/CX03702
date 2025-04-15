733c5722e474c24eb449522337a26b833e43dfd6
hxasset <hxasset0307@gmail.com>
2025-04-15 14:35:37 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,323 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-l",
+      "name": "L",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.1915008224168044,
+        "pitch": -0.12006647529375236,
+        "fov": 1.0301879842183064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.687731954040661,
+          "pitch": 0.21430463977066339,
+          "rotation": 6.283185307179586,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.238148713333743,
+          "pitch": 0.17364991035985433,
+          "title": "匯幸資產版權所有",
+          "text": "<202504拍攝>"
+        },
+        {
+          "yaw": 0.0,
+          "pitch": 0.0,
+          "title": "格局圖",
+          "text": "<img src='floorplan.jpg' style='width:100%;' />"
+        }
+      ]
+    },
+    {
+      "id": "1-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.7630789710456725,
+        "pitch": 0.3176277868403723,
+        "fov": 1.0301879842183064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8154976993591916,
+          "pitch": 0.10874534267113489,
+          "rotation": 0,
+          "target": "3-3"
+        },
+        {
+          "yaw": -2.585382080193078,
+          "pitch": 0.0579596455446314,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": 2.28338506925677,
+          "pitch": 0.24050544985528077,
+          "rotation": 0,
+          "target": "0-l"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0,
+          "pitch": 0.0,
+          "title": "格局圖",
+          "text": "<img src='floorplan.jpg' style='width:100%;' />"
+        }
+      ]
+    },
+    {
+      "id": "2-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.6515484998071335,
+        "pitch": -0.009353997639841793,
+        "fov": 1.0301879842183064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.10778174958552,
+          "pitch": 0.04605959761840772,
+          "rotation": 0,
+          "target": "5-5"
+        },
+        {
+          "yaw": 2.8549498591592197,
+          "pitch": -0.016900596674865653,
+          "rotation": 4.71238898038469,
+          "target": "4-4"
+        },
+        {
+          "yaw": 1.6581145441938911,
+          "pitch": 0.21488308730229733,
+          "rotation": 0,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0,
+          "pitch": 0.0,
+          "title": "格局圖",
+          "text": "<img src='floorplan.jpg' style='width:100%;' />"
+        }
+      ]
+    },
+    {
+      "id": "3-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.8939609242632223,
+        "pitch": 0.091389133515829,
+        "fov": 1.0301879842183064
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0123622085088755,
+          "pitch": 0.0272505004715633,
+          "rotation": 0.7853981633974483,
+          "target": "2-2"
+        },
+        {
+          "yaw": 0.6073417575780713,
+          "pitch": 0.026392994713784645,
+          "rotation": 0,
+          "target": "1-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0,
+          "pitch": 0.0,
+          "title": "格局圖",
+          "text": "<img src='floorplan3.jpg' style='width:100%;' />"
+        }
+      ]
+    },
+    {
+      "id": "4-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.9257479244531055,
+        "pitch": 0.07965827872218156,
+        "fov": 1.0301879842183064
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0738684826370077,
+          "pitch": 0.17971526903386525,
+          "rotation": 0,
+          "target": "2-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0,
+          "pitch": 0.0,
+          "title": "格局圖",
+          "text": "<img src='floorplan.jpg' style='width:100%;' />"
+        }
+      ]
+    },
+    {
+      "id": "5-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.534122530843125,
+        "pitch": -0.030112358555362206,
+        "fov": 1.0301879842183064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7713347590101787,
+          "pitch": 0.0637456360255868,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": -0.5197186683926844,
+          "pitch": 0.007406040297148309,
+          "rotation": 1.5707963267948966,
+          "target": "4-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0,
+          "pitch": 0.0,
+          "title": "格局圖",
+          "text": "<img src='floorplan5.jpg' style='width:100%;' />"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
